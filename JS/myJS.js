220eff3d338fc880db182f6395c0000aabfdf964
vi-ht <huynhthanhvi1102@gmail.com>
2022-10-27 23:45:24 +0700

    Update responsive for home page

--- JS/myJS.js
+++ JS/myJS.js
@@ -16,11 +16,16 @@ const productList =
     { name: "Costumes Fashion Pet Clother Cowboy Rider", product: "Costume", size: "1.5kg", price: "500.000 VND", image: "ASSET/Product6.png", content: "Free Toy & Free Shake" },
     { name: "Costumes Chicken Drumsti ck Headband", product: "Costume", price: "400.000 VND", image: "ASSET/Product7.png", content: "Free Cat Food" },
     { name: "Plush Pet Toy", product: "Toy", price: "250.000 VND", image: "ASSET/Product8.png", content: "Free Food & Free Shake" }];
+const knowledgeList =
+    [{ name: "What is a Pomeranian? How to Identify Pomeranian Dogs", content: "The Pomeranian, also known as the Pomeranian (Pom dog), is always in the top of the cutest pets. Not only that, the small, lovely, smart, friendly, and skillful...", image: "ASSET/knowledge1.png" },
+    { name: "Dog Diet You Need To Know", content: "Dividing a dog's diet may seem simple at first, but there are some rules you should know so that your dog can easily absorb the nutrients in the diet...", image: "ASSET/knowledge2.png" }, ,
+    { name: "Why Dogs Bite and Destroy Furniture and How to Prevent It Effectively", content: "Dog bites are common during development. However, no one wants to see their furniture or important items being bitten by a dog.", image: "ASSET/knowledge3.png" },];
 
 // var declaration
 
-    const add = doglist.map(addDogToList);
-    const addProduct = productList.map(addProductToList);
+const add = doglist.map(addDogToList);
+const addProduct = productList.map(addProductToList);
+const knowledge = knowledgeList.map(addKnowledgeToList);
 
 // function 
 
@@ -58,15 +63,31 @@ function addProductToList(value) {
     document.getElementById('list_of_product').insertAdjacentHTML('beforeend', html);
 }
 
-// get scroll position
-// var position = document.getElementById('position'); 
-// position.innerHTML = "" 
+function addKnowledgeToList(value) {
+    var html =
+        '<div class="col-lg-4 col-md-4 p-2 ">'
+        + '<div class="card card-style p-2">'
+        + '<img src=' + value.image + ' class="card-img-top" alt="...">'
+        + '<div class="card-body pb-0 ps-1 pe-1">'
+        + '<h5><span class="badge rounded-pill text-bg-primary fw-normal p-2">Pet knowledge</span></h5>'
+        + '<h5 class="card-title dog-name lh-base">' + `${value.name.length < 30 ? value.name + '<span style="color:white">add whitespace to end</span>' : value.name}` + '</h5>'
+        + '<p class="dog-info d-inline lh-1" style="text-overflow:ellipsis">' + value.content + '</p>'
+        + '</div>'
+        + '</div>'
+        + '</div>';
+    document.getElementById('list_of_knowledge').insertAdjacentHTML('beforeend', html);
+}
 
 window.addEventListener("scroll", function (event) {
-
     var scroll_y = this.scrollY;
-    var scroll_x = this.scrollX;
-    // console.log(scroll_x, scroll_y); 
-    // position.innerHTML = " X-axis : " 
-    // + scroll_x + " và Y-axis : " + scroll_y 
-}); 
+    if(scroll_y > 0){
+        document.getElementById("bg-nav").style.display = "block";
+        document.getElementById("pageLogo").style.width = "110px";
+        document.getElementById("pageLogo").style.height = "40px";
+    }else{
+        document.getElementById("bg-nav").style.display = "none";
+        document.getElementById("pageLogo").style.width = "130px";
+        document.getElementById("pageLogo").style.height = "50px";
+    }
+        
+});
