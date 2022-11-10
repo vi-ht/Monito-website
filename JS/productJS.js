933acdad7a8af7e42902232dfca5f0afa46da3d3
vi-ht <huynhthanhvi1102@gmail.com>
2022-11-11 00:46:47 +0700

    fix error

--- JS/productJS.js
+++ JS/productJS.js
@@ -4,10 +4,8 @@ import { changeNav, changeIcon } from "../JS/navbar.js";
 // call function ------------------------------------------------------
 window.addEventListener("scroll", changeNav);
 changeIcon();
-
 // global var -----------------------------------------------------------------
 
-// const addProduct = ;
 document.getElementById("total").innerHTML = productList.length > 1 ? productList.length + " products" : productList.length + " product";
 // add dog card to list function -----------------------------------------------
 productList.map((product) => {
@@ -29,8 +27,45 @@ productList.map((product) => {
     let cardName = document.createElement("h5");
     cardName.className = "card-title dog-name";
     cardName.innerText = product.name;
-    //add cardName to info container
+    //create product type 
+    let cardType = document.createElement("p");
+    cardType.innerText = "Product: " + product.product;
+    cardType.className = "dog-info d-inline";
+    //create cardPrice
+    let cardPrice = document.createElement("p");
+    cardPrice.innerText = product.price;
+    cardPrice.className = "dog-name fw-semibold mt-2 mb-1";
+    // create card gift 
+    let cardGift = document.createElement("div");
+    cardGift.id = "present_tag";
+    cardGift.className = "container rounded";
+    // create gif iconify
+    let icon = document.createElement("iconify-icon");
+    icon.setAttribute("icon", "fxemoji:present");
+    icon.style = "color: #103559;"
+    icon.setAttribute("width", "24");
+    icon.setAttribute("heigth", "24");
+    icon.className = "mt-2";
+    // create gif content
+    let content = document.createElement("span");
+    content.className = "position-relative ms-2";
+    content.innerText = product.content;
+    //------------------------------------------------------------------------
+    //add icon, content to cardGif
+    cardGift.appendChild(icon);
+    cardGift.appendChild(content);
+    //add cardName, cardType to info container
     cardInfo.appendChild(cardName);
+    cardInfo.appendChild(cardType);
+    if (product.size !== undefined) {
+        //create product size 
+        let cardSize = document.createElement("p");
+        cardSize.innerText = " - Size: " + product.size;
+        cardSize.className = "dog-info d-inline";
+        cardInfo.appendChild(cardSize);
+    }
+    cardInfo.appendChild(cardPrice);
+    cardInfo.appendChild(cardGift);
     //add cardImage and cardInfo to productCard
     productCard.appendChild(cardImage);
     productCard.appendChild(cardInfo);
@@ -39,6 +74,7 @@ productList.map((product) => {
     //add container to list of product
     document.getElementById('list_of_product').appendChild(container);
 })
+// productList.map(addProductToList)
 // function addProductToList(value) {
 //     var html =
 //         '<div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 p-2">'
